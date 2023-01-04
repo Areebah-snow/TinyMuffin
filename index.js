931e6d88b89d95b0b2e121e4ec61567459483bee
Areebah-snow <Areebah-snow>
2023-01-04 23:50:05 +0100

    Getting localStorage ready

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-let myLeads = []
+let myLeads = ["www.amazonn.com"]
+
+myLeads = JSON.parse(myLeads)
+
+myLeads.push("www.coco.com")
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+
+
+
+
+
+
+
+
+
+
+
+
 
 inputBtn.addEventListener("click", function(){
    myLeads.push(inputEl.value)
@@ -14,15 +30,14 @@ inputBtn.addEventListener("click", function(){
 function renderLeads(){
     let listItems =  ""
     for(let i = 0; i < myLeads.length; i++){
-         listItems += `<li><a target='_blank' href=' ${myLeads[i]}'>
+         listItems += `
+         <li><a target='_blank' href=' ${myLeads[i]}'>
                             ${myLeads[i]}
                         </a></li>`
       
         
          console.log(listItems)
-        // const list = document.createElement("li")
-        // list.textContent = myLeads[i]
-        // ulEl.append(list)
+        
  
  ulEl.innerHTML =listItems
 }
