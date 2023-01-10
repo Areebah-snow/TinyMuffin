efa06940f581b0b8529bfada4111c445f1db7a00
Areebah-snow <Areebah-snow>
2023-01-10 10:08:11 +0100

    Checking status

--- index.js
+++ index.js
@@ -1,48 +1,54 @@
-let myLeads = ["www.amazonn.com"]
-
-myLeads = JSON.parse(myLeads)
-
-myLeads.push("www.coco.com")
-
-
-
-
+let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const tabBtn = document.getElementById("tab-btn")
+if(leadsFromLocalStorage){
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
 
 
+tabBtn.addEventListener("click", function(){
+    // chrome.tabs.query({
+     
+    // )
+    chrome.tabs.query({active : true, currentWindow: true},
+        function(tabs){
+            console.log(tabs)
+            myLeads.push(tabs[0].url)
+            localStorage.setItem("myLeads", JSON.stringify(myLeads))
+            render(myLeads)
+        })
+})
+function render(leads){
+    let listItems =  ""
+    for(let i = 0; i < leads.length; i++){
+         listItems += `
+             <li><a target='_blank' href=' ${leads[i]}'>
+                            ${leads[i]}
+             </a></li>`
+    }
+    ulEl.innerHTML =listItems                                                                                                                            
+}
 
-
-
-
-
-
-
-
-
-
+deleteBtn.addEventListener("dblclick", function(){
+      localStorage.clear()
+      myLeads = []
+      render(myLeads)
+})
 
 inputBtn.addEventListener("click", function(){
-   myLeads.push(inputEl.value)
+   myLeads.push(inputEl.value) 
    //Clears input field
         inputEl.value = ""
    //Display items 
-   renderLeads()
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+
+    //verify it works
+   console.log(localStorage.getItem("myLeads"))
 })
 
-function renderLeads(){
-    let listItems =  ""
-    for(let i = 0; i < myLeads.length; i++){
-         listItems += `
-         <li><a target='_blank' href=' ${myLeads[i]}'>
-                            ${myLeads[i]}
-                        </a></li>`
-      
-        
-         console.log(listItems)
-        
- 
- ulEl.innerHTML =listItems
-}
-}
